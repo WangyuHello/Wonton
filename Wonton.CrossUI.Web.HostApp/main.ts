9ad3bf96dac71433929cf2aef3eca1ed67318aa9
王渝 <wwangyuu@outlook.com>
2020-04-04 16:27:07 +0800

    更新ClientApp依赖

--- Wonton.CrossUI.Web.HostApp/main.ts
+++ Wonton.CrossUI.Web.HostApp/main.ts
@@ -98,7 +98,7 @@ app.on('activate', () => {
     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
     // 通常在应用程序中重新创建一个窗口。
     if (BrowserWindow.getAllWindows().length === 0) {
-        //createWindow();
+        createWindow();
         startBackend();
     }
 });
