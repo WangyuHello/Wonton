734acf04d17acec91b44c095dedb5560fe29ea19
王渝 <wwangyuu@outlook.com>
2020-10-22 18:55:00 +0800

    更新拷贝脚本

--- PackageManager/github/CopyToRelease.ps1
+++ PackageManager/github/CopyToRelease.ps1
@@ -4,11 +4,18 @@ Param(
     [string]$Release = "Build"
 )
 
-Write-Host "Copy packages from $From to $Release"
+Write-Host "从 $From 拷贝到 $Release"
 
 Copy-Item -Path "$From/**/*.7z" -Destination $Release -Force
 Copy-Item -Path "$From/**/*.deb" -Destination $Release -Force
 Copy-Item -Path "$From/**/*.dmg" -Destination $Release -Force
 Copy-Item -Path "$From/**/*.exe" -Destination $Release -Force
 
-Get-ChildItem -Path $Release
+$pkgs = Get-ChildItem -Path $Release
+$count = $pkgs.Count
+
+Write-Host "总包数量: $count"
+foreach ($item in $pkgs) {
+    $name = $item.Name
+    Write-Host "$name"
+}
