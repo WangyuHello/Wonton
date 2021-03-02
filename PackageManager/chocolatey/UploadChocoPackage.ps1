95f985a01290e0454e8c73aa408200764d8224a4
王渝 <wwangyuu@outlook.com>
2021-03-02 21:10:40 +0800

    更新Chocolatey脚本

--- PackageManager/chocolatey/UploadChocoPackage.ps1
+++ PackageManager/chocolatey/UploadChocoPackage.ps1
@@ -7,6 +7,10 @@ Param(
 if ($API_KEY -eq "") {
     $API_KEY = $env:CHOCOLATEY_API_KEY
 }
+if ($API_KEY -eq "") {
+    Write-Host "No API KEY found"
+    exit 1
+}
 
 $ProjectRoot = (Get-Item $PSScriptRoot).Parent.Parent.FullName
 $pkgJsonFile = Get-Content "$ProjectRoot/Wonton.CrossUI.Web.HostApp/package.json" -Raw
@@ -26,7 +30,8 @@ if ($cur_tag -and $cur_tag.StartsWith("v")) {
     Write-Host "Tag found: $cur_tag, Uploading"
     $cur = Get-Location
     Set-Location "$PSScriptRoot/wonton"
-    Invoke-Expression "choco push wonton.$Version.nupkg -k $API_KEY -s https://push.chocolatey.org/"
+    Get-ChildItem "."
+    Invoke-Expression "choco push -k $API_KEY -s https://push.chocolatey.org/"
     Set-Location $cur
 } else {
     Write-Host "No tag matched. Skip uploading"
